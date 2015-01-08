1c4dd83e63e6c29a8548400882d8fcc3cde86097
heavenshell <heavenshell.jp@gmail.com>
2015-01-08 15:29:25 +0900

    Add test.

--- tests/test_gyroscope.py
+++ tests/test_gyroscope.py
@@ -61,3 +61,59 @@ class TestGyroscope(TestCase):
         self.assertTrue(os.path.exists('{0}/test.txt.3'.format(self.current)))
         self.gyroscope.run('{0}/test.txt'.format(self.current), 3)
         self.assertFalse(os.path.exists('{0}/test.txt.4'.format(self.current)))
+
+    def test_do_rotate_5(self):
+        #: Create file
+        def create_file(p, data):
+            with open(p, 'w') as f:
+                f.write(data)
+
+        def assert_not_same_file(src, dest):
+            self.assertTrue(os.path.getsize(src) != os.path.getsize(dest))
+
+        def assert_same_file(src, dest):
+            self.assertTrue(os.path.getsize(src) == os.path.getsize(dest))
+
+        def clean(file):
+            if os.path.exists(file):
+                os.remove(file)
+
+        filepath = os.path.join(self.current, 'dummy.obj')
+        clean(filepath)
+        clean(os.path.join(self.current, 'dummy.obj.1'))
+        clean(os.path.join(self.current, 'dummy.obj.2'))
+        clean(os.path.join(self.current, 'dummy.obj.3'))
+
+        create_file(filepath, 'test')
+        self.gyroscope.run(filepath, 3)
+
+        #: Original file and original.1 is same.
+        assert_same_file(filepath,
+                         os.path.join(self.current, 'dummy.obj.1'))
+
+        create_file(filepath, 'test' * 3)
+        self.gyroscope.run(filepath, 3)
+
+        assert_same_file(filepath,
+                         os.path.join(self.current, 'dummy.obj.1'))
+        assert_not_same_file(os.path.join(self.current, 'dummy.obj.1'),
+                             os.path.join(self.current, 'dummy.obj.2'))
+
+        create_file(filepath, 'test' * 5)
+        self.gyroscope.run(filepath, 3)
+
+        assert_same_file(filepath,
+                         os.path.join(self.current, 'dummy.obj.1'))
+        assert_not_same_file(os.path.join(self.current, 'dummy.obj.1'),
+                             os.path.join(self.current, 'dummy.obj.2'))
+        assert_not_same_file(os.path.join(self.current, 'dummy.obj.2'),
+                             os.path.join(self.current, 'dummy.obj.3'))
+
+        self.gyroscope.run(filepath, 3)
+        assert_same_file(filepath,
+                         os.path.join(self.current, 'dummy.obj.1'))
+        assert_not_same_file(os.path.join(self.current, 'dummy.obj.1'),
+                             os.path.join(self.current, 'dummy.obj.2'))
+        assert_not_same_file(os.path.join(self.current, 'dummy.obj.2'),
+                             os.path.join(self.current, 'dummy.obj.3'))
+
